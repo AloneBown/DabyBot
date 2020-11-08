177871ae8b9dcfce2713cbdb0b271a44ebadaf46
AloneBown <74094680+AloneBown@users.noreply.github.com>
2020-11-08 09:41:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -154,6 +154,19 @@ async def help_ping(ctx):
     emb.add_field(name='Назначение', value = 'Самая простая развлекательная команда')
     
     await ctx.send(embed=emb)
+
+@bot.command()
+async def help_ping(ctx):
+    await ctx.channel.purge(limit=1)
+    
+    emb = discord.Embed(title = 'Помощь по команде: ping', colour=discord.Colour.green())
+
+    emb.set_author(name = bot.user.name, icon_url= bot.user.avatar_url)
+    emb.add_field( name = 'Команда'.format(PREFIXx), value = '{}say')
+    emb.add_field(name='Назначение', value = 'Сказать что-то через бота')
+    emb.add_field(name='Пример', value = '!say "Random text"')
+    
+    await ctx.send(embed=emb)
 #-----------------------------------------------------ВЫДАЧА ОШИБОК-------------------------------------------------------#
 @clear.error
 async def clear_error(ctx, error):
