521122bc00112028d2e466f433c8bc3e9b004056
AloneBown <74094680+AloneBown@users.noreply.github.com>
2020-11-08 17:52:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,15 +31,14 @@ async def on_message(message):
     if msg in bad_words:
         await message.delete()
     
-@bot.event()  
-#@commands.Cog.listener()
-async def on_raw_reaction_add(payload: RawReactionActionEvent):
-    if not payload.message_id == 774998564678008832:  # ID сообщения на которое нужно ставить реакции
-        return
-    if not payload.emoji.name == "1️⃣":  # или payload.emoji.name == "✔" для unicode-эмодзей
-        return
-    if member : payload.member:
-        await member.add_roles(member.guild.get_role(775013337548587008))
+@client.event
+async def on_reaction_add(reaction, user):
+  ChID = '774997225167257642'
+  if reaction.message.channel.id != ChID:
+    return
+  if reaction.emoji == "1️⃣":
+    CSGO = discord.utils.get(user.server.roles, name="Minecraft")
+    await client.add_roles(user, CSGO)
 #Пинг понг
 @bot.command()
 async def ping(ctx):
